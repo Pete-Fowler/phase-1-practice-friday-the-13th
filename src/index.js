2b41ca267da55aac9e68cd6c79623cb8e310c652
Pete Fowler <fowler.pete@gmail.com>
2022-08-17 10:27:06 -0600

    Add images to nav list

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-
+const addImages = (movies) => {
+  const list = document.querySelector('#movie-list');
+  movies.forEach(movie => {
+    const img = document.createElement('img');
+    img.src = movie.image;
+    list.append(img);
+  });
+}
 
 const getMovies = () => {
   fetch(`http://localhost:3000/movies`)
-  .then(res => res.json());
-  // .then(data => addImages(data))
+  .then(res => res.json())
+  .then(data => addImages(data));
   // .then(data => addDetails(data))
 
 }
